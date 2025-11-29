9234d9d8ff8f45785b159f982dfc8c67f3ff7e0a
muyuu <anticyborg@gmail.com>
2025-11-29 13:21:20 +0900

    feat: 最初に20ウェーブ作成する

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -58,8 +58,10 @@ impl AppState {
         let players = data.setup_state.players.clone();
 
         let mut game = Game::new(area, players);
-        game.add_wave(); // 最初のターンを作成
-        game.add_wave(); // 2番目のターンを作成
+        // とりあえず20ウェーブ作成
+        for _ in 0..20 {
+            game.add_wave();
+        }
         data.game = Some(game);
         data.current_wave_index = 0;
         data.show_setup_dialog = false;
