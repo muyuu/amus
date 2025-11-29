1a9ee422a8c922e9cadbd6b94f5f7af0000e32e3
muyuu <anticyborg@gmail.com>
2025-11-29 14:54:23 +0900

    feat: Locationに定数を作った

---
+++ src/features/location/constants.rs
@@ -0,0 +1,13 @@
+pub struct LocationConstants;
+
+impl LocationConstants {
+    pub const SPAWN_LOCATION_SIZE: f32 = 28.0;
+    pub const SPAWN_LOCATION_STROKE_WIDTH: f32 = 3.0;
+    pub const SPAWN_LOCATION_HIT_SIZE: f32 = 24.0;
+
+    pub const END_LOCATION_SIZE: f32 = 28.0;
+    pub const END_LOCATION_STROKE_WIDTH: f32 = 3.0;
+    pub const END_LOCATION_HIT_SIZE: f32 = 24.0;
+    pub const END_LOCATION_DEAD_MARK_SIZE: f32 = 24.0;
+    pub const END_LOCATION_DEAD_MARK_STROKE_WIDTH: f32 = 6.0;
+}
