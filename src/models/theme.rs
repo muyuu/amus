e830a870c1465c7607e2a0541ae6e00d731f54fa
muyuu <anticyborg@gmail.com>
2025-12-05 09:57:27 +0900

    feat: マップ画像を更新
    -利用可能と明言されてる方の作成した画像に差し替え
        - https://baskmedia.jp/amongus-japanese-map/
    - テーマを追加
        - 今は dark しか使ってない

---
+++ src/models/theme.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Theme {
+    Light,
+    Dark,
+}
+
+impl Theme {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Theme::Light => "light",
+            Theme::Dark => "dark",
+        }
+    }
+}
