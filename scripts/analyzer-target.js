acac0ec76db46f5f73b77b5e52df0f539926f1e0
muyuu <anticyborg@gmail.com>
2025-12-06 19:41:09 +0900

    biuld: rust-analyzer のターゲット切り替えを wasm もできるようにした

--- scripts/analyzer-target.js
+++ scripts/analyzer-target.js
@@ -1,7 +1,7 @@
 /**
  * amus.code-workspace を編集して rust-analyzer のオプションを変更する
  * 引数: release or debug (default: debug)
- */ 
+ */
 
 import * as fs from "fs";
 import * as path from "path";
@@ -10,35 +10,41 @@ import JSON5 from "json5";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const ALLOWED_TARGETS = ["release", "debug"];
+const ALLOWED_TARGETS = ["release", "debug", "wasm"];
 
-function getTargetArg() {
+function getTargetAndExtraArgs() {
     const arg = process.argv[2] || "debug";
 
     if (!ALLOWED_TARGETS.includes(arg)) {
-        console.error("ターゲット指定が正しくないです。 'release' か 'debug' で指定して。");
+        console.error(`ターゲット指定が正しくないです。 ${ALLOWED_TARGETS.map(t => `'${t}'`).join(" か ")} で指定して。`);
         process.exit(1);
     }
 
-    return arg === 'debug' ? null : `--${arg}`;
+    let target = arg;
+    if (arg === 'wasm') {
+        target = 'wasm32-unknown-unknown';
+    }
+
+    return { target, arg: arg === 'debug' ? null : arg };
 }
 
 try {
-    const target = getTargetArg();
+    const { target, arg } = getTargetAndExtraArgs();
     const workspacePath = path.join(__dirname, "..", "amus.code-workspace");
     const workspaceJson = JSON5.parse(fs.readFileSync(workspacePath, "utf-8"));
 
     // settings に rust-analyzer.cargo.extraArgs を追加または更新
-    if (!workspaceJson.settings && target !== null) {
-        workspaceJson.settings = { "rust-analyzer.cargo.extraArgs": [target] };
+    if (Object.keys(workspaceJson).indexOf("settings") === -1) {
+        console.log("settings セクションが存在しないため、新規作成します");
+        workspaceJson.settings = {};
     }
 
-    if (workspaceJson.settings && target === null) {
-        delete workspaceJson.settings["rust-analyzer.cargo.extraArgs"];
-    }
-
-    if (workspaceJson.settings && target !== null) {
-        workspaceJson.settings["rust-analyzer.cargo.extraArgs"] = [target];
+    workspaceJson.settings["rust-analyzer.cargo.extraArgs"] = [`--${arg}`];
+    
+    if (arg === 'wasm') {
+        workspaceJson.settings["rust-analyzer.cargo.target"] = target;
+    } else {
+        delete workspaceJson.settings["rust-analyzer.cargo.target"];
     }
 
     // 変更をファイルに書き戻す
