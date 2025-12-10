99e9f02381c5cb39a5942bbc2de0b9c5089c17b9
muyuu <anticyborg@gmail.com>
2025-12-10 17:07:29 +0900

    refactor: プレイヤーリストに定数を追加

---
+++ src/features/player_list/constants.rs
@@ -0,0 +1,5 @@
+pub struct PlayerInfoConstants;
+impl PlayerInfoConstants {
+    pub const TILE_SIZE: f32 = 50.0;
+    pub const TILE_GAP: f32 = 6.0;
+}
--- src/features/player_list/view.rs
+++ src/features/player_list/view.rs
@@ -1,3 +1,4 @@
+use super::PlayerInfoConstants as Constants;
 use crate::{
     components::{tile, TileConf},
     models::Player,
@@ -27,8 +28,8 @@ impl PlayerListView {
             None => return,
         };
 
-        let player_width = 50.0; // 各プレイヤーの幅
-        let player_gap = 6.0; // 各プレイヤー間のギャップ
+        let player_width = Constants::TILE_SIZE;
+        let player_gap = Constants::TILE_GAP;
         let total_content_width =
             players.len() as f32 * player_width + (players.len() as f32 - 1.0) * player_gap;
         let available_width = available_rect.width();
@@ -75,7 +76,7 @@ impl PlayerListView {
                 &TileConf {
                     color: player.color.to_egui_color(),
                     label: Some(player.name.clone()),
-                    size: None,
+                    size: Some(Vec2::new(Constants::TILE_SIZE, Constants::TILE_SIZE)),
                     is_selected,
                     is_dragging,
                 },
